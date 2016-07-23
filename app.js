bdddb21ec687d170ec440ca9827ce3c7b230aa01
emerson <emerson91030@gmail.com>
2016-07-22 19:31:01 -0700

    Add routing

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
-var marsApp = angular.module("marsApp", []);
+// modules
+var marsApp = angular.module("marsApp", ["ngRoute",]);
 
+// routes
+marsApp.config(function($routeProvider) {
+	$routeProvider
+	.when("/", {
+		templateUrl: "pages/home.html",
+		controller: "homeController",
+	})
+});
+
+// controllers
 marsApp.controller("homeController", ["$scope", function($scope) {
 	$scope.page = "home";
 	console.log($scope.page);
